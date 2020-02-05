4b21fb12cbbe4005a7b045bf5664ccdca8d30e0b
Caro <carolynatterbury@gmail.com>
2020-02-05 15:37:19 -0700

    added impurity file

---
+++ Impurity.py
@@ -0,0 +1,32 @@
+import math
+
+# type: integer. 0 to calculate missclassificationError, 1 to calculate entropy, 2 to calculate giniIndex
+def impurity(type, data):
+	if (type == 0):
+		missclassificationError(data)
+	elif (type == 1):
+		entropy(data)
+	else:
+		giniIndex(data)
+
+def missclassificationError(data):
+# hmmmmm
+
+
+# data: array of probabilities ?
+def entropy(data):
+
+	ent = 0
+	for p in data:
+		ent += -1 * p * (math.log(p))
+	return ent
+
+# data: array of probabilities ?
+def giniIndex(data):
+
+	sum = 0;
+	for p in data:
+		sum += p ** 2
+
+	return 1 - sum
+
