d72036b21dd4ce6ea042c48e3d0bec121bd48e5c
Caro <carolynatterbury@gmail.com>
2020-02-09 15:29:41 -0700

    switched to spaces

--- impurity.py
+++ impurity.py
@@ -2,31 +2,30 @@ import math
 
 # type: integer. 0 to calculate missclassificationError, 1 to calculate entropy, 2 to calculate giniIndex
 def impurity(type, data):
-	if (type == 0):
-		missclassificationError(data)
-	elif (type == 1):
-		entropy(data)
-	else:
-		giniIndex(data)
+    if (type == 0):
+        missclassificationError(data)
+    elif (type == 1):
+        entropy(data)
+    else:
+        giniIndex(data)
 
 def missclassificationError(data):
-# hmmmmm
+        return 0
 
 
 # data: array of probabilities ?
 def entropy(data):
-
-	ent = 0
-	for p in data:
-		ent += -1 * p * (math.log(p))
-	return ent
+    ent = 0
+    for p in data:
+        ent += -1 * p * (math.log(p))
+    return ent
 
 # data: array of probabilities ?
 def giniIndex(data):
 
-	sum = 0;
-	for p in data:
-		sum += p ** 2
+    sum = 0;
+    for p in data:
+        sum += p ** 2
 
-	return 1 - sum
+    return 1 - sum
 
--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 import Tree
-import Impurity
+import impurity
 import pickle
 
 def main():
-	d = []#  read data from csv
-
-	t = dt_construct(d)
-
-	## Then pickle tree
-	## dump pickled tree into file 
-	file = open('decision_tree',  'wb')
-	pickle.dump(t, file)
-	file.close()
-
-
-def dt_construct(d):
+    d = []#  read data from csv
     t = Tree()
+    #`` t = dt_construct(d,t)
+    ## Then pickle tree
+    ## dump pickled tree into file
+    file = open('decision_tree',  'wb')
+    pickle.dump(t, file)
+    file.close()
+
+
+def dt_construct(d,t):
 
     t.data = representative_class(d) 
     if impure(d):
